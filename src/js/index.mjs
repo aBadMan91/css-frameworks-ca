2c364b2535a4990bca077efb69d89355a939c8f2
aBadMan <114421846+aBadMan91@users.noreply.github.com>
2023-11-21 19:00:17 +0100

    Added CRUD posts

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,5 +1,6 @@
 import { setRegisterFormListener } from "./handlers/register.mjs";
 import { setLoginFormListener } from "./handlers/login.mjs";
+// import * as post from "./api/posts/index.mjs";
 
 const path = location.pathname;
 
@@ -8,3 +9,11 @@ if (path === "/profile/login/") {
 } else if (path === "/profile/register/") {
   setRegisterFormListener();
 }
+
+// post.createPost();
+// post.updatePost();
+// post.removePost();
+// post.getPost();
+// post.getPosts().then(console.log);
+
+// post.getPost(8820).then(console.log);
