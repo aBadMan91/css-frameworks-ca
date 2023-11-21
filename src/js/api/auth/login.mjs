e43afbe9d366c83fb0012161bc3acf3d5c22f7eb
aBadMan <114421846+aBadMan91@users.noreply.github.com>
2023-11-21 17:14:47 +0100

    Completed auth code

--- src/js/api/auth/login.mjs
+++ src/js/api/auth/login.mjs
@@ -1,20 +1,26 @@
 import { API_SOCIAL_URL } from "../constants.mjs";
+import * as storage from "../../storage/index.mjs";
 
 const action = "/auth/login";
 const method = "post";
 
 export async function login(profile) {
-    const loginURL = API_SOCIAL_URL + action;
-    const body = JSON.stringify(profile);
+  const loginURL = API_SOCIAL_URL + action;
+  const body = JSON.stringify(profile);
 
-    const response = await fetch (loginURL, {
-        headers: {
-            "Content-Type": "application/json"
-        },
-        method,
-        body
-    })
+  const response = await fetch(loginURL, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method,
+    body,
+  });
 
-    const result = await response.json()
-    console.log(result)
+  const { accessToken, ...user } = await response.json();
+
+  storage.save("token", accessToken);
+
+  storage.save("profile", user);
+
+  alert("You are now logged in");
 }
