c4405a57984c39e6fc3fcc82b60f211f3137da5d
aBadMan <114421846+aBadMan91@users.noreply.github.com>
2023-12-04 16:33:37 +0100

    Adds router with switch logic

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,25 +1,3 @@
-import * as listeners from "./handlers/index.mjs";
-import * as templates from "./templates/index.mjs";
-import * as postMethods from "./api/posts/index.mjs";
+import router from "./router.mjs";
 
-const path = location.pathname;
-
-if (path === "/profile/login/") {
-  listeners.setLoginFormListener();
-} else if (path === "/profile/register/") {
-  listeners.setRegisterFormListener();
-} else if (path === "/post/create/") {
-  listeners.setCreatePostFormListener();
-} else if (path === "/post/edit/") {
-  listeners.setUpdatePostFormListener();
-} else if (path === "/profile/edit/") {
-  listeners.setUpdateProfileListener();
-}
-
-// async function testTemplate() {
-//   const posts = await postMethods.getPosts();
-//   const container = document.querySelector("#post");
-//   templates.renderPostTemplates(posts, container);
-// }
-
-// testTemplate();
+router();
